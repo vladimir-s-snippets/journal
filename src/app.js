56cada63a2424cefe68c75d1575d98aec45ff857
Vladimir Statsenko <vovchiksta@gmail.com>
2017-01-24 15:59:49 +0200

    Events added

--- src/app.js
+++ src/app.js
@@ -2,20 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class UserForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            departmentId: props.departments[0].id,
+            fio: ''
+        };
+        this.handleDepartmentChange = this.handleDepartmentChange.bind(this);
+        this.handleFIOChange = this.handleFIOChange.bind(this);
+        this.handleAddUser = this.handleAddUser.bind(this);
+    }
+
+    handleDepartmentChange(event) {
+        this.setState({departmentId: parseInt(event.target.value)});
+    }
+
+    handleFIOChange(event) {
+        this.setState({fio: event.target.value});
+    }
+
+    handleAddUser(event) {
+        console.log(this.state);
+        event.preventDefault();
+    }
+
     render() {
         let departments = this.props.departments.map((department) => {
-            return (<option value={department.id}>{department.name}</option>);
+            return (<option key={department.id} value={department.id}>{department.name}</option>);
         });
         return (
             <form>
                 <p>
-                    <select>
+                    <select value={this.state.departmentId} onChange={this.handleDepartmentChange}>
                         {departments}
                     </select>
                 </p>
                 <p>
-                    <input type="text" placeholder="FIO" />
-                    <button>Добавить пользователя</button>
+                    <input type="text" placeholder="FIO" value={this.state.fio} onChange={this.handleFIOChange} />
+                    <button onClick={this.handleAddUser}>Добавить пользователя</button>
                 </p>
             </form>
         );
